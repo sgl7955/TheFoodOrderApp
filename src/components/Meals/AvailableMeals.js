41edf606de4c1764a80cd22d8b40d11838c9982c
Gibok <sgl7955@gmail.com>
2023-03-24 16:33:13 +0900

    Meal 컴포넌트 양식 추가

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,50 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
   {
     id: "m1",
     name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
+    description: "최고급 생선으로 만나는 일본!",
+    price: 22.9,
   },
   {
     id: "m2",
     name: "Schnitzel",
-    description: "A german specialty!",
+    description: "독일의 명물!",
     price: 16.5,
   },
   {
     id: "m3",
     name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
+    description: "미국의 맛!",
+    price: 12.9,
   },
   {
     id: "m4",
     name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
+    description: "건강 그리고 초록",
+    price: 18.9,
   },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
